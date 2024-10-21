8392b6d693cd966a9a25759258d2d0f3fd7ed8d0
TylerHester <tylerhester44@yahoo.co.uk>
2024-10-21 13:04:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ route_coords2 = route2['features'][0]['geometry']['coordinates']
 route_coords3 = route3['features'][0]['geometry']['coordinates']
 route_coords4 = route4['features'][0]['geometry']['coordinates']
 st.markdown(route_coords)
+
 noise_points=pd.read_csv("noise_points.csv")
 df_restaurant=pd.read_csv("df_restaurant.csv")
 df_owners=pd.read_csv("df_owners.csv")
@@ -48,63 +49,63 @@ df_fast_uk=df_restaurant[df_restaurant["category"] == "fast_food"]
                          
 
 
-map_potential_sites = Map(location=[noise_points['latitude'].mean(), noise_points['longitude'].mean()], zoom_start=5)
+# map_potential_sites = Map(location=[noise_points['latitude'].mean(), noise_points['longitude'].mean()], zoom_start=5)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+# PolyLine(
+#     locations=[(coord[1], coord[0]) for coord in route_coords],
+#     color='blue', weight=5, opacity=0.8,
+# ).add_to(map_potential_sites)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords2],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+# PolyLine(
+#     locations=[(coord[1], coord[0]) for coord in route_coords2],
+#     color='blue', weight=5, opacity=0.8,
+# ).add_to(map_potential_sites)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords3],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+# PolyLine(
+#     locations=[(coord[1], coord[0]) for coord in route_coords3],
+#     color='blue', weight=5, opacity=0.8,
+# ).add_to(map_potential_sites)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords4],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+# PolyLine(
+#     locations=[(coord[1], coord[0]) for coord in route_coords4],
+#     color='blue', weight=5, opacity=0.8,
+# ).add_to(map_potential_sites)
 
-Marker(belfast, popup="Belfast START", icon=Icon(color='green')).add_to(map_potential_sites) #The Causeway
-Marker(londonderry, popup="Londonderry END", icon=Icon(color='red')).add_to(map_potential_sites)
+# Marker(belfast, popup="Belfast START", icon=Icon(color='green')).add_to(map_potential_sites) #The Causeway
+# Marker(londonderry, popup="Londonderry END", icon=Icon(color='red')).add_to(map_potential_sites)
 
-Marker(bideford, popup="Bideford START", icon=Icon(color='green')).add_to(map_potential_sites) #The Atlantic Highway
-Marker(newquay, popup="Newquay END", icon=Icon(color='red')).add_to(map_potential_sites)
+# Marker(bideford, popup="Bideford START", icon=Icon(color='green')).add_to(map_potential_sites) #The Atlantic Highway
+# Marker(newquay, popup="Newquay END", icon=Icon(color='red')).add_to(map_potential_sites)
 
-Marker(kendal, popup="Kendal START", icon=Icon(color='green')).add_to(map_potential_sites) #Kendal to Keswick
-Marker(keswick, popup="Keswick END", icon=Icon(color='red')).add_to(map_potential_sites)
+# Marker(kendal, popup="Kendal START", icon=Icon(color='green')).add_to(map_potential_sites) #Kendal to Keswick
+# Marker(keswick, popup="Keswick END", icon=Icon(color='red')).add_to(map_potential_sites)
 
-Marker(st_davids, popup="St Davids Church START", icon=Icon(color='green')).add_to(map_potential_sites) #The Coastal Way
-Marker(st_hywyn, popup="St Hywyns Church END", icon=Icon(color='red')).add_to(map_potential_sites)
+# Marker(st_davids, popup="St Davids Church START", icon=Icon(color='green')).add_to(map_potential_sites) #The Coastal Way
+# Marker(st_hywyn, popup="St Hywyns Church END", icon=Icon(color='red')).add_to(map_potential_sites)
 
-marker_cluster = MarkerCluster().add_to(map_potential_sites)
-marker_cluster2 = MarkerCluster().add_to(map_potential_sites)
-
-
-heat_data = [[row['latitude'], row['longitude']] for index, row in noise_points.iterrows()]
+# marker_cluster = MarkerCluster().add_to(map_potential_sites)
+# marker_cluster2 = MarkerCluster().add_to(map_potential_sites)
 
 
-for index, row in df_fast_uk.iterrows():
-    Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['restaurant_name'],
-        icon=Icon(color='green', icon='cutlery')
-    ).add_to(marker_cluster)
-
-for index, row in df_owners.iterrows():
-    Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['ID'],
-        icon=Icon(color='blue', icon='bolt')
-    ).add_to(marker_cluster2)
+# heat_data = [[row['latitude'], row['longitude']] for index, row in noise_points.iterrows()]
 
 
+# for index, row in df_fast_uk.iterrows():
+#     Marker(
+#         location=[row['latitude'], row['longitude']],
+#         popup=row['restaurant_name'],
+#         icon=Icon(color='green', icon='cutlery')
+#     ).add_to(marker_cluster)
+
+# for index, row in df_owners.iterrows():
+#     Marker(
+#         location=[row['latitude'], row['longitude']],
+#         popup=row['ID'],
+#         icon=Icon(color='blue', icon='bolt')
+#     ).add_to(marker_cluster2)
 
 
-HeatMap(heat_data).add_to(map_potential_sites)
-map_potential_sites
+
+
+# HeatMap(heat_data).add_to(map_potential_sites)
+# map_potential_sites

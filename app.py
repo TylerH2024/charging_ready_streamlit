37de95b97ef03705c3bb6ac48d7a8adf21509240
TylerHester <tylerhester44@yahoo.co.uk>
2024-10-22 14:50:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from streamlit_folium import st_folium
+import streamlit as st
 import pandas as pd
 from folium import Map, Icon, PolyLine, Marker, CircleMarker
-import streamlit as st
-import openrouteservice
 from folium.plugins import MarkerCluster, HeatMap
-
+from streamlit_folium import st_folium
+import openrouteservice
 
 API_KEY = st.secrets["API_KEY"]
 
@@ -14,122 +13,126 @@ def get_client():
 
 client = get_client()
 
-st.title("Charging Ready")
-
-@st.cache_data
-def get_route(_client, coordinates):
-    return _client.directions(coordinates=coordinates, profile='driving-car', format='geojson')
-
-kendal = (54.3280, -2.7460)
-keswick = (54.5994, -3.1340)
-st_davids = (51.882916, -5.264581)
-st_hywyn = (52.804869, -4.710975)
-bideford = (51.014885, -4.213305)
-newquay = (50.415365, -5.067994)
-belfast = (54.599899, -5.944937)
-londonderry = (55.019594, -7.341320)
-
-coordinates = [kendal[::-1], keswick[::-1]]
-coordinates2 = [st_davids[::-1], st_hywyn[::-1]]
-coordinates3 = [bideford[::-1], newquay[::-1]]
-coordinates4 = [belfast[::-1], londonderry[::-1]]
-
-route = get_route(client, coordinates)
-route2 = get_route(client, coordinates2)
-route3 = get_route(client, coordinates3)
-route4 = get_route(client, coordinates4)
-
-def convert_coords(coords):
-    return [[coord[1], coord[0]] for coord in coords]
-
-route_coords = route['features'][0]['geometry']['coordinates']
-route_coords2 = route2['features'][0]['geometry']['coordinates']
-route_coords3 = route3['features'][0]['geometry']['coordinates']
-route_coords4 = route4['features'][0]['geometry']['coordinates']
-
 @st.cache_data
 def load_data():
     noise_points = pd.read_csv("noise_points.csv")
     df_fast_uk = pd.read_csv("df_fast_food_combined.csv")
-    df_owners = pd.read_csv("df_owners.csv")
     noise_points1 = pd.read_csv("noise_points (1).csv")
-    return noise_points, df_fast_uk, df_owners, noise_points1
+    def load_parquet("df_owners.csv"):
+    return pd.read_parquet("df_owners.csv")
+    return noise_points, df_fast_uk, noise_points1
+    
 
-noise_points, df_fast_uk, df_owners, noise_points1 = load_data()
+def create_route_map(df_fast_uk, df_owners, noise_points):
+    kendal = (54.3280, -2.7460)
+    keswick = (54.5994, -3.1340)
+    st_davids = (51.882916, -5.264581)
+    st_hywyn = (52.804869, -4.710975)
+    bideford = (51.014885, -4.213305)
+    newquay = (50.415365, -5.067994)
+    belfast = (54.599899, -5.944937)
+    londonderry = (55.019594, -7.341320)
 
-map_potential_sites = Map(location=[noise_points['latitude'].mean(), noise_points['longitude'].mean()], zoom_start=5)
+    
+    coordinates = [kendal[::-1], keswick[::-1]]
+    coordinates2 = [st_davids[::-1], st_hywyn[::-1]]
+    coordinates3 = [bideford[::-1], newquay[::-1]]
+    coordinates4 = [belfast[::-1], londonderry[::-1]]
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+   
+    route = client.directions(coordinates=coordinates, profile='driving-car', format='geojson')
+    route2 = client.directions(coordinates=coordinates2, profile='driving-car', format='geojson')
+    route3 = client.directions(coordinates=coordinates3, profile='driving-car', format='geojson')
+    route4 = client.directions(coordinates=coordinates4, profile='driving-car', format='geojson')
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords2],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+    
+    map_potential_sites = Map(location=[noise_points['latitude'].mean(), noise_points['longitude'].mean()], zoom_start=5)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords3],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+    
+    for route in [route, route2, route3, route4]:
+        route_coords = route['features'][0]['geometry']['coordinates']
+        PolyLine(
+            locations=[(coord[1], coord[0]) for coord in route_coords],
+            color='blue', weight=5, opacity=0.8,
+        ).add_to(map_potential_sites)
 
-PolyLine(
-    locations=[(coord[1], coord[0]) for coord in route_coords4],
-    color='blue', weight=5, opacity=0.8,
-).add_to(map_potential_sites)
+    
+    start_end_points = [
+        (belfast, "Belfast START", 'green'),
+        (londonderry, "Londonderry END", 'red'),
+        (bideford, "Bideford START", 'green'),
+        (newquay, "Newquay END", 'red'),
+        (kendal, "Kendal START", 'green'),
+        (keswick, "Keswick END", 'red'),
+        (st_davids, "St Davids Church START", 'green'),
+        (st_hywyn, "St Hywyns Church END", 'red')
+    ]
 
-Marker(belfast, popup="Belfast START", icon=Icon(color='green')).add_to(map_potential_sites)
-Marker(londonderry, popup="Londonderry END", icon=Icon(color='red')).add_to(map_potential_sites)
+    for point in start_end_points:
+        Marker(point[0], popup=point[1], icon=Icon(color=point[2])).add_to(map_potential_sites)
 
-Marker(bideford, popup="Bideford START", icon=Icon(color='green')).add_to(map_potential_sites)
-Marker(newquay, popup="Newquay END", icon=Icon(color='red')).add_to(map_potential_sites)
+    
+    marker_cluster = MarkerCluster().add_to(map_potential_sites)
 
-Marker(kendal, popup="Kendal START", icon=Icon(color='green')).add_to(map_potential_sites)
-Marker(keswick, popup="Keswick END", icon=Icon(color='red')).add_to(map_potential_sites)
+    
+    for index, row in df_fast_uk.iterrows():
+        Marker(
+            location=[row['latitude'], row['longitude']],
+            popup=row['restaurant_name'],
+            icon=Icon(color='green', icon='cutlery')
+        ).add_to(map_potential_sites)
 
-Marker(st_davids, popup="St Davids Church START", icon=Icon(color='green')).add_to(map_potential_sites)
-Marker(st_hywyn, popup="St Hywyns Church END", icon=Icon(color='red')).add_to(map_potential_sites)
+    
+    for index, row in df_owners.iterrows():
+        Marker(
+            location=[row['latitude'], row['longitude']],
+            popup=row['ID'],
+            icon=Icon(color='blue', icon='car', prefix="fa")
+        ).add_to(marker_cluster)
 
-marker_cluster = MarkerCluster().add_to(map_potential_sites)
+    
+    heat_data = [[row['latitude'], row['longitude']] for index, row in noise_points.iterrows()]
+    HeatMap(heat_data).add_to(map_potential_sites)
 
-heat_data = [[row['latitude'], row['longitude']] for index, row in noise_points.iterrows()]
+    return map_potential_sites
 
-for index, row in df_fast_uk.iterrows():
-    Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['restaurant_name'],
-        icon=Icon(color='green', icon='cutlery')
-    ).add_to(map_potential_sites)
 
-for index, row in df_owners.iterrows():
-    Marker(
-        location=[row['latitude'], row['longitude']],
-        popup=row['ID'],
-        icon=Icon(color='blue', icon='car', prefix="fa")
-    ).add_to(marker_cluster)
+def create_noise_map(noise_points1):
+    option = st.selectbox("Select Cluster", (-1, 2, 4, 3))
+    noise_subcluster = noise_points1[noise_points1['sub_cluster'] == option]
 
-HeatMap(heat_data).add_to(map_potential_sites)
-st_folium(map_potential_sites, key="map_1")
+    map_noise = Map(
+        location=[noise_subcluster['latitude'].mean(), noise_subcluster['longitude'].mean()],
+        zoom_start=10
+    )
 
-option = st.selectbox("Select Cluster", (-1, 2, 4, 3))
+    for idx, row in noise_subcluster.iterrows():
+        CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=5,
+            color='red',
+            fill=True,
+            fill_color='red',
+            fill_opacity=0.6,
+            popup=f"Potential Charging Location at ({row['latitude']}, {row['longitude']})"
+        ).add_to(map_noise)
 
-noise_subcluster = noise_points1[noise_points1['sub_cluster'] == option]
+    return map_noise
 
-map_noise = Map(
-    location=[noise_subcluster['latitude'].mean(), noise_subcluster['longitude'].mean()],
-    zoom_start=10
-)
 
-for idx, row in noise_subcluster.iterrows():
-    CircleMarker(
-        location=[row['latitude'], row['longitude']],
-        radius=5,
-        color='red',
-        fill=True,
-        fill_color='red',
-        fill_opacity=0.6,
-        popup=f"Potential Charging Location at ({row['latitude']}, {row['longitude']})"
-    ).add_to(map_noise)
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Select Page", ("Charging Routes", "Noise Points"))
 
-st_folium(map_noise, key=f"subcluster_map_{option}")
+
+if page == "Charging Routes":
+    st.title("Charging Routes and EV Owners")
+    noise_points, df_fast_uk, df_owners, noise_points1 = load_data()
+    map_potential_sites = create_route_map(df_fast_uk, df_owners, noise_points)
+    st_folium(map_potential_sites, key="map_1")
+
+
+elif page == "Noise Points":
+    st.title("Potential Charging Locations based on Noise Data")
+    _, _, _, noise_points1 = load_data()  # Re-load noise points data
+    map_noise = create_noise_map(noise_points1)
+    st_folium(map_noise, key="map_noise")

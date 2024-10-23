384eb303886988699bd4aa9665e601a502c071a4
TylerHester <tylerhester44@yahoo.co.uk>
2024-10-23 11:56:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -31,22 +31,18 @@ def create_route_map(df_fast_uk, df_owners, noise_points):
     belfast = (54.599899, -5.944937)
     londonderry = (55.019594, -7.341320)
 
-    
     coordinates = [kendal[::-1], keswick[::-1]]
     coordinates2 = [st_davids[::-1], st_hywyn[::-1]]
     coordinates3 = [bideford[::-1], newquay[::-1]]
     coordinates4 = [belfast[::-1], londonderry[::-1]]
 
-   
     route = client.directions(coordinates=coordinates, profile='driving-car', format='geojson')
     route2 = client.directions(coordinates=coordinates2, profile='driving-car', format='geojson')
     route3 = client.directions(coordinates=coordinates3, profile='driving-car', format='geojson')
     route4 = client.directions(coordinates=coordinates4, profile='driving-car', format='geojson')
 
-    
     map_potential_sites = Map(location=[noise_points['latitude'].mean(), noise_points['longitude'].mean()], zoom_start=5)
 
-    
     for route in [route, route2, route3, route4]:
         route_coords = route['features'][0]['geometry']['coordinates']
         PolyLine(
@@ -54,7 +50,6 @@ def create_route_map(df_fast_uk, df_owners, noise_points):
             color='blue', weight=5, opacity=0.8,
         ).add_to(map_potential_sites)
 
-    
     start_end_points = [
         (belfast, "Belfast START", 'green'),
         (londonderry, "Londonderry END", 'red'),
@@ -69,10 +64,8 @@ def create_route_map(df_fast_uk, df_owners, noise_points):
     for point in start_end_points:
         Marker(point[0], popup=point[1], icon=Icon(color=point[2])).add_to(map_potential_sites)
 
-    
     marker_cluster = MarkerCluster().add_to(map_potential_sites)
 
-    
     for index, row in df_fast_uk.iterrows():
         Marker(
             location=[row['latitude'], row['longitude']],
@@ -80,44 +73,46 @@ def create_route_map(df_fast_uk, df_owners, noise_points):
             icon=Icon(color='green', icon='cutlery')
         ).add_to(map_potential_sites)
 
-    
     for index, row in df_owners.iterrows():
         Marker(
             location=[row['latitude'], row['longitude']],
             icon=Icon(color='blue', icon='car', prefix="fa")
         ).add_to(marker_cluster)
 
-    
     heat_data = [[row['latitude'], row['longitude']] for index, row in noise_points.iterrows()]
     HeatMap(heat_data).add_to(map_potential_sites)
 
     return map_potential_sites
 
-   
-
-noise_points1 = pd.read_csv("noise_points (1).csv")
-df_owners=pd.read_csv("df_owners_new.csv")
-df_fast_uk = pd.read_csv("df_fast_food_combined.csv")
-def create_noise_map(noise_points1):
+def create_noise_map(noise_points1, df_owners, df_fast_uk):
     option = st.selectbox("Select Cluster", (-1, 2, 4, 3))
     noise_subcluster = noise_points1[noise_points1['sub_cluster'] == option]
-    marker_cluster = MarkerCluster().add_to(map_noise)
+
+    
     map_noise = Map(
         location=[noise_subcluster['latitude'].mean(), noise_subcluster['longitude'].mean()],
         zoom_start=10
     )
 
+    
+    marker_cluster = MarkerCluster().add_to(map_noise)
+
+    
     for index, row in df_owners.iterrows():
         Marker(
             location=[row['latitude'], row['longitude']],
             icon=Icon(color='blue', icon='car', prefix="fa")
         ).add_to(marker_cluster)
+
+    
     for index, row in df_fast_uk.iterrows():
         Marker(
             location=[row['latitude'], row['longitude']],
             popup=row['restaurant_name'],
             icon=Icon(color='green', icon='cutlery')
-        ).add_to(map_potential_sites)
+        ).add_to(marker_cluster)
+
+    
     for idx, row in noise_subcluster.iterrows():
         CircleMarker(
             location=[row['latitude'], row['longitude']],
@@ -131,7 +126,6 @@ def create_noise_map(noise_points1):
 
     return map_noise
 
-
 st.sidebar.title("Navigation")
 page = st.sidebar.radio("Select Page", ("Potential Charging Locations", "Clustered Charging Locations"))
 
@@ -143,6 +137,7 @@ if page == "Potential Charging Locations":
 
 elif page == "Clustered Charging Locations":
     st.title("Potential Charging Locations - Scotland, Northern Ireland, Isle of Man")
-    _, _, _, noise_points1 = load_data() 
-    map_noise = create_noise_map(noise_points1)
+    noise_points, df_fast_uk, df_owners, noise_points1 = load_data()
+    map_noise = create_noise_map(noise_points1, df_owners, df_fast_uk)
     st_folium(map_noise, key="map_noise")
+
